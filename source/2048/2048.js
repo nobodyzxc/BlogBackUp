fdb800a1af45b7cb808e6ea6119e149698489149
nobodyzxc <dici86.chen@gmail.com>
2017-04-21 21:03:48 +0800

    backup

--- source/2048/2048.js
+++ source/2048/2048.js
@@ -167,3 +167,79 @@ window.addEventListener('keydown', function(event) {
     }
 } , false);
 
+
+
+//返回角度
+function GetSlideAngle(dx, dy) {
+    returnMath.atan2(dy, dx) * 180 / Math.PI;
+}
+
+//根据起点和终点返回方向 1：向上，2：向下，3：向左，4：向右,0：未滑动
+function GetSlideDirection(startX, startY, endX, endY) {
+    var dy = startY - endY;
+    var dx = endX - startX;
+    varresult = 0;
+
+    //如果滑动距离太短
+    if(Math.abs(dx) < 2 && Math.abs(dy) < 2) {
+        returnresult;
+    }
+
+    varangle = GetSlideAngle(dx, dy);
+    if(angle >= -45 && angle < 45) {
+        result = 4;
+    }else if (angle >= 45 && angle < 135) {
+        result = 1;
+    }else if (angle >= -135 && angle < -45) {
+        result = 2;
+    }
+    else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
+        result = 3;
+    }
+
+    returnresult;
+}
+
+//滑动处理
+var startX, startY;
+document.addEventListener('touchstart',function (ev) {
+    startX = ev.touches[0].pageX;
+    startY = ev.touches[0].pageY;
+}, false);
+
+document.addEventListener('touchend',function (ev) {
+
+    var moveUnit = function(dir){ return function(y , x){ if(map[y][x] != 0) map[y][x].move(dir); }; }
+
+    var endX, endY;
+    endX = ev.changedTouches[0].pageX;
+    endY = ev.changedTouches[0].pageY;
+    var direction = GetSlideDirection(startX, startY, endX, endY);
+    switch(direction) {
+        case 0:
+            alert("没滑动");
+            break;
+        case 1:
+            forMap(0 , 1 , 0 , 1 , moveUnit(1));
+            break;
+        case 2:
+            forMap(size - 1 , -1 , 0 , 1 , moveUnit(3));
+            break;
+        case 3:
+            forMap(0 , 1 , 0 , 1 , moveUnit(0));
+            alert("!");
+            break;
+        case 4:
+            forMap(0 , 1 , size - 1 , -1 , moveUnit(2));
+            break;
+        default:
+    }
+
+    if(moved === true){ addBck(rand(0 , 2)) ,  moved = false; }
+    forMap(0 , 1 , 0 , 1 , function(y , x){ if(map[y][x] != 0) map[y][x].lock = false; });
+    if(pass === false && checkGameOver()){
+        forMap(0 , 1 , 0 , 1 , function(y , x){ if(map[y][x] != 0){ score += (2 << map[y][x].pt); } });
+        pass = true , setTimeout(function() { alert("GameOver! score: " + String(score)); },10);
+    }
+
+}, false);
