8e304987b815b83d7223e3d32319293a8b6b52ce
nobodyzxc <dici86.chen@gmail.com>
2019-10-21 17:02:31 +0800

    backup

--- source/files/ukagaka.js
+++ source/files/ukagaka.js
@@ -13,9 +13,11 @@ var aliveInterval = 30000;
 var killInterval = 300001; // 5 min
 
 function new_box(){
+  $('#ukagaka_reconnect').text('連接中請稍等...');
   if(box === undefined){
     box = new WebSocket(ws_url);
     box.onopen = function(){
+
       $('#ukagaka_reconnect').hide();
       $('#ukagaka_addstring').show();
       $('#ukagaka_addstring').focus();
@@ -593,3 +595,28 @@ $(window).on("load", function(){
   widget.insertBefore(node, widget.firstChild);
   init(node, default_option);
 });
+
+/*
+
+L2Dwidget
+        .on('*', (name) => {
+          console.log('%c EVENT ' + '%c -> ' + name, 'background: #222; color: yellow', 'background: #fff; color: #000')
+        })
+        .init({
+          dialog: {
+            // 开启对话框
+            enable: true,
+            script: {
+              // 每空闲 10 秒钟，显示一条一言
+              'every idle 10s': '$hitokoto$',
+              // 当触摸到星星图案
+              'hover .star': '星星在天上而你在我心里 (* /ω＼*)',
+              // 当触摸到角色身体
+              'tap body': '哎呀！别碰我！',
+              // 当触摸到角色头部
+              'tap face': '人家已经不是小孩子了！'
+            }
+          }
+        });
+
+*/
